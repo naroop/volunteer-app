823425d44ffbe157b68c5a1c464aaa979d280830
naroop <76455626+naroop@users.noreply.github.com>
2023-04-10 20:22:18 -0500

    elastic ip

--- ui/src/main.js
+++ ui/src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "http://18.220.2.121:8000/"
+    ? "http://18.218.117.37:8000/"
     : "http://127.0.0.1:8000/";
 
 createApp(App).use(router).mount("#app");
